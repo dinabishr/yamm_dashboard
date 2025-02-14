c1985f009eab1c1fd0aeff60f4f7ebb0f69463cf
Dina Bishr <dinabishr78@gmail.com>
2025-02-14 17:20:16 +0300

    Initial react+ts project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app-container">
+      <main className="content">
+        <h1>Welcome to My Project</h1>
+      </main>
+    </div>
+  );
+}
+
+export default App;
